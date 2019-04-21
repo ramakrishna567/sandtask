d756df7849d56d7f3a04d5f5dbf0b3544f4de39a
Rama Krishna M <m.ramkrishna567@gmail.com>
2019-04-21 14:21:16 +0530

    sand task completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, Validators, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'sandtask';
+  // searchName = new FormControl('');
+  // isInactive: boolean = false;
+
+  constructor(private fb: FormBuilder) { }
+
+  searchForm = this.fb.group({
+    searchName: [''],
+    isInactive: [false]
+  })
+
+  get searchName() { return this.searchForm.get('searchName'); }
+  get username() { return this.searchForm.get('username'); }
+
+  sendDate() {
+    console.log("searchForm", this.searchForm.value);
+
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,9 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatCardModule, MatIconModule, MatInputModule, MatFormFieldModule, MatButtonModule, MatDividerModule, MatCheckboxModule } from '@angular/material';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -10,7 +13,17 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatIconModule,
+    MatInputModule,
+    MatFormFieldModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatButtonModule,
+    MatDividerModule,
+    MatCheckboxModule
   ],
   providers: [],
   bootstrap: [AppComponent]
